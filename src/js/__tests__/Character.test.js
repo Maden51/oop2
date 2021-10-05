c1e9233c3b7b64ed93e700e7379b3e10874a0b39
Maden5126 <madenon123@gmail.com>
2021-10-05 16:15:35 +0300

    1.0.1

--- src/js/__tests__/Character.test.js
+++ src/js/__tests__/Character.test.js
@@ -21,3 +21,31 @@ test('should create a Character', () => {
   };
   expect(newPlayer).toEqual(expected);
 });
+
+test('should throw error when trying levelup with 0 hp', () => {
+  const newPlayer = new Character('Oleg', 'Bowman', 25, 25);
+  newPlayer.health = 0;
+  expect(() => { newPlayer.levelUp(); }).toThrow('Нельзя повысить уровень умершего');
+});
+
+test('should lvlup and increase stats', () => {
+  const newPlayer = new Character('Igor', 'Swordsman', 40, 10);
+  newPlayer.health = 80;
+  newPlayer.levelUp();
+  const expected = {
+    name: 'Igor',
+    type: 'Swordsman',
+    health: 100,
+    level: 2,
+    attack: 48,
+    defence: 12,
+  };
+  expect(newPlayer).toEqual(expected);
+});
+
+test('should leave 18 health', () => {
+  const newPlayer = new Character('Bodya', 'Zombie', 40, 10);
+  newPlayer.damage(20);
+  const expected = newPlayer.health;
+  expect(expected).toBe(18);
+});
